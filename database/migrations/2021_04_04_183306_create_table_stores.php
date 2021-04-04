bb6ded76e549bba713ce70a2c9310ba81716d9d6
mikaelle <mikaelle.rubiaps02@gmail.com>
2021-04-04 19:21:11 -0300

    tabela stores

---
+++ database/migrations/2021_04_04_183306_create_table_stores.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableStores extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stores', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
+            $table->string('name');
+            $table->string('phone');
+            $table->string('mobile_phone');
+            $table->string('slug');
+
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');// nome da chave estran:stores_user_id_foreign
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stores');
+    }
+}
